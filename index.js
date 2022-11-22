395837400eb33e6125621c59cd58cb471056eeec
nagibnur <nagibnur@hotmail.com>
2022-11-22 11:12:23 +0000

    added initial questions to README

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log('hello world');
+
+the title of my project 
+and sections entitled Description,
+Table of Contents, 
+Installation,
+Usage,
+License, 
+Contributing,
+Tests,
+and Questions

8260835c43c92bae066f238206d2fc0160220512
nagibnur <nagibnur@hotmail.com>
2022-11-22 11:24:28 +0000

    put my questions into loop and display on node

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-console.log('hello world');
+const Questions = [
+    'title',
+    'Description',
+    'Contents',
+    'Installation',
+    'Usage',
+    'License',
+    'Contributing',
+    'Tests',
+    'Questions',
+]
 
-the title of my project 
-and sections entitled Description,
-Table of Contents, 
-Installation,
-Usage,
-License, 
-Contributing,
-Tests,
-and Questions
+const inquirerQuestions = Questions.map(it => {
+    return {
+        type: 'input',
+        name: it,
+        Message: it,
+    }
+})
+console.log(inquirerQuestions);
+
+// the title of my project 
+// and sections entitled Description,
+// Table of Contents, 
+// Installation,
+// Usage,
+// License, 
+// Contributing,
+// Tests,
+// and Questions

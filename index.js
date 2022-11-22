b114c67d7d8eb891c8a32d08a02e6b6528c45c45
nagibnur <nagibnur@hotmail.com>
2022-11-22 12:16:37 +0000

    replace first templated value

--- index.js
+++ index.js
@@ -1,61 +1,61 @@
-const inquirer = require('inquirer')
-const fs = require('fs')
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 const Questions = [
-    'title',
-    'Description',
-    'Contents',
-    'Installation',
-    'Usage',
-    'License',
-    'Contributing',
-    'Tests',
-    'Questions',
-]
+  "title",
+  "Description",
+  "Contents",
+  "Installation",
+  "Usage",
+  "License",
+  "Contributing",
+  "Tests",
+  "Questions",
+];
 
-const inquirerQuestions = Questions.map(it => {
-    return {
-        type: 'input',
-        name: it,
-        Message: it,
-    }
-})
+const inquirerQuestions = Questions.map((it) => {
+  return {
+    type: "input",
+    name: it,
+    Message: it,
+  };
+});
 
 async function askQuestions() {
-  return await  inquirer.prompt(inquirerQuestions)
+  return await inquirer.prompt(inquirerQuestions);
 }
 
 async function collectAnswersAndGeneratorREADME() {
-    const responses = await askQuestions()
-    console.log(responses);
+  const responses = await askQuestions();
+  console.log(responses);
 }
 async function generatorReadme(answers) {
-   const READMEDocument = await fs.promises.readFile('./README.md', 'utf8')
-   console.log(READMEDocument);
+  const READMEDocument = await fs.promises.readFile("./README.md", "utf8");
+  const READMEfile = READMEDocument.replace("title", answers.title);
+  console.log(READMEfile);
 }
 
 const dummyAnswers = {
-    title: 'nagib',
-    Description: 'readme',
-    Contents: 'homework',
-    Installation: 'now',
-    Usage: 'dont know',
-    License: 'not yet',
-    Contributing: 'my self',
-    Tests: 'go',
-    Questions: 'done'
-  }
-  
+  title: "nagib",
+  Description: "readme",
+  Contents: "homework",
+  Installation: "now",
+  Usage: "dont know",
+  License: "not yet",
+  Contributing: "my self",
+  Tests: "go",
+  Questions: "done",
+};
 
-generatorReadme(dummyAnswers)
+generatorReadme(dummyAnswers);
 // collectAnswersAndGeneratorREADME()
 
-// the title of my project 
+// the title of my project
 // and sections entitled Description,
-// Table of Contents, 
+// Table of Contents,
 // Installation,
 // Usage,
-// License, 
+// License,
 // Contributing,
 // Tests,
 // and Questions
